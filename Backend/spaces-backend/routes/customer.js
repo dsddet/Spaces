63108d7ac36e9dc7a2c9081aabb7eebbec87d5a6
Dawit Woldegiorgis <dwoldegiorgis@mum.edu>
2018-12-15 20:41:33 -0600

    update customer router

--- Backend/spaces-backend/routes/customer.js
+++ Backend/spaces-backend/routes/customer.js
@@ -1,9 +1,8 @@
 const router = require ('express').Router();
 
-// get users by id (email)
+// get user by id
 router.get('user/customer/:id',function(req,res) {
-    const collection = req.db.collection('users');
-    collection.findOne({"_id":req.params.id},function (err,user){
+    req.db.collection('users').findOne({"_id":req.params.id},function (err,user){
         if (err) {
             res.send(204);
         } else {
@@ -12,7 +11,7 @@ router.get('user/customer/:id',function(req,res) {
     });
 });
 
-// add user
+// add user to database
 router.post('/user/customer', function (req,res){
     req.db.collection('users').insert(req.body, (err,user) => {
         if(err) {
@@ -28,7 +27,7 @@ router.post('/user/customer', function (req,res){
 // update user information with specified id
 // request should have a properly formatted user data that can be added to the database 
 router.patch('user/customer/:id', function(req,res){
-    req.db.collection('user').update({_id:req.params.id}, {$set:req.body}, (err,user) => {
+    req.db.collection('user').findOneAndUpdate({_id:req.params.id}, {$set:req.body}, (err,user) => {
         if (err) {
             // set error header
             res.send(204);
@@ -41,7 +40,7 @@ router.patch('user/customer/:id', function(req,res){
 
 // delete user with specified id
 router.delete('user/customer/:id', function(req,res){
-    req.db.collection('user').delete({_id:req.params.id}, (err, user) => { 
+    req.db.collection('user').findOneAndRemove({_id:req.params.id}, (err, user) => { 
         if (err) {
             // set error header
             res.send(204, "No user found with id: " + req.params.id);
