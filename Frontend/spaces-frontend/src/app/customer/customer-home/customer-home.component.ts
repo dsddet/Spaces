3578a3547ff7a34744642041c4a66bbcbd36605a
Dawit Woldegiorgis <dawithw@gmail.com>
2018-12-16 16:05:34 -0600

    create customer homepage

---
+++ Frontend/spaces-frontend/src/app/customer/customer-home/customer-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'customer-home',
+  templateUrl: './customer-home.html',
+  styleUrls: ['./customer-home.css']
+})
+export class CustomerHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
