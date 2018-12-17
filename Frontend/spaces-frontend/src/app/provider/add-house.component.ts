b1aa449eef7f6fb1069b736ad1677ac56960125f
Birhane Tamene <birefifty@gmail.com>
2018-12-16 23:57:07 -0600

    implemented GET and POST services of Provider module

--- Frontend/spaces-frontend/src/app/provider/add-house.component.ts
+++ Frontend/spaces-frontend/src/app/provider/add-house.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { ObjectUnsubscribedError } from 'rxjs';
+import { getInjectorIndex } from '@angular/core/src/render3/di';
 
 @Component({
   selector: 'add-house',
@@ -10,7 +12,29 @@ import { HttpClient } from '@angular/common/http';
   encapsulation: ViewEncapsulation.None
 })
 export class AddHouseComponent implements OnInit {
-  house = {};
+  
+  house = {
+    "_id":Math.random,
+    "Name": "",
+    "Address": {
+        "Street": "",
+        "City": "",
+        "State": "",
+        "Zip": ""
+    },
+    "Details": {
+        "Bedrooms": "",
+        "Bathrooms": "",
+        "Size": "",
+        "Price": "",
+        "Amenities": ""
+    },
+    "Status": "pending",
+    "Provider": "provider1@email.com",
+    "DateCreated": Date.now(),
+    "DateModified": Date.now(),
+    "ReservedBy": null
+};
   constructor(private http:HttpClient, private router:Router) { }
 
   ngOnInit() {
@@ -19,8 +43,7 @@ export class AddHouseComponent implements OnInit {
   addHome() {
     this.http.post('http://localhost:4201/provider/listings', this.house)
       .subscribe(res => {
-          let id = res['_id'];
-          this.router.navigate(['/provider/addhouse', id]);
+          this.router.navigate(['/provider/addhouse']);
         }, (err) => {
           console.log(err);
         }
--- Frontend/spaces-frontend/src/app/provider/provider.component.ts
+++ Frontend/spaces-frontend/src/app/provider/provider.component.ts
@@ -4,11 +4,9 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'provider-home',
   templateUrl: './provider.component.html',
-  //template: `{{houses}}`,
   styleUrls:['./provider.component.css']
 })
 export class ProviderComponent implements OnInit{
- //houses= ['house1','house2','house3','house4','house5','house6','house7'];
  constructor(private http: HttpClient){}
  houses: any;
 
